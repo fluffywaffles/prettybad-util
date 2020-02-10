8fbf2c4c92af05549a4d360b3d4b0790dfd5ae64
fluffywaffles <skorlir@gmail.com>
2020-02-09 19:33:32 -0500

    Chore: update sisyphus to v0.3.0

--- test.mjs
+++ test.mjs
@@ -1,3 +1,3 @@
 import { test } from './index.mjs'
-import { sisyphus, reporters } from '@sisyphus/sisyphus'
-test(sisyphus({ reporter: reporters.simple }))
+import * as sisyphus from '@sisyphus/sisyphus'
+test(sisyphus.Harness({ reporter: sisyphus.reporters.simple }))
