4589ff672dbabc18536fea87729a52b9dff33711
fluffywaffles <skorlir@gmail.com>
2020-04-05 23:09:33 -0400

    Refactor/Major: time: use `performance` instead of `process.hrtime`
    - Platform-independent: works in browsers
    - Uses doubles and millisecond-precision instead of bigints and nanosecs
    - Forces `time` to be asynchronous
    
    Asynchronous profiling may be a frustrating limitation when trying to
    instrument (poorly isolated) code, but `time` will not be particularly
    applicable to that use-case in the first place, seeing as it has always
    been designed to take and evaluate a (synchronous) thunk.
    
    It may be desirable to later refactor `time` to accept promise thunks,
    and `await` them in an async function.

--- test.mjs
+++ test.mjs
@@ -2,6 +2,6 @@ import { time, test } from './lib/index.mjs'
 import * as sisyphus from '@sisyphus/sisyphus'
 
 const harness = sisyphus.Harness({ reporter: sisyphus.reporters.simple })
-const profile = time(() => test(harness))
 
-console.log(profile.summarize({}))
+time(() => test(harness))
+  .then(profile => console.log(profile.summarize({})))
